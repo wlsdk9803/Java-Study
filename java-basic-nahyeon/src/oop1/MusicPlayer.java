6e81f83ed8b543a11ecb4b5b4cf5b38abc42d4e6
kim nahyeon <stome6322@inha.edu>
2024-04-28 14:59:42 +0900

    [week2] : 객체 지향 프로그래밍 - 캡슐화

---
+++ java-basic-nahyeon/src/oop1/MusicPlayer.java
@@ -0,0 +1,38 @@
+package oop1;
+
+public class MusicPlayer {
+    //음악 플레이어에 필요한 속성과 기능을 한 클래스에 정의
+
+    int volume = 0;
+    boolean isOn = false;
+
+    void on() {
+        isOn = true;
+        System.out.println("음악 플레이어를 시작합니다.");
+    }
+
+    void off() {
+        isOn = false;
+        System.out.println("음악 플레이어를 종료합니다.");
+    }
+
+    void volumeUp() {
+        volume++;
+        System.out.println("음악 플레이어 볼륨: " + volume);
+    }
+
+    void volumeDown() {
+        volume--;
+        System.out.println("음악 플레이어 볼륨: " + volume);
+    }
+
+    void showStatus() {
+        System.out.println("음악 플레이어 상태 확인");
+
+        if(isOn) {
+            System.out.println("음악 플레이어 ON, 불륨: " + volume);
+        } else {
+            System.out.println("음악 플레이어 OFF");
+        }
+    }
+}
