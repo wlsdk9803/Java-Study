8c46b640423861712998410ef09351337adbb1c9
kim nahyeon <stome6322@inha.edu>
2024-04-29 22:45:30 +0900

    [week2] : 생성자 - 오버로딩과 this()

--- java-basic-nahyeon/src/construct/MemberConstruct.java
+++ java-basic-nahyeon/src/construct/MemberConstruct.java
@@ -5,6 +5,14 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    MemberConstruct(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//        this.grade = 50;
+        this(name, age, 50);
+        //생성자 내부에서 자신의 생성자를 호출
+    }
+
     MemberConstruct(String name, int age, int grade) {
         System.out.println("생성자 호출 name=" + name + ", age= " + age + ", grade= " + grade);
         this.name = name;
