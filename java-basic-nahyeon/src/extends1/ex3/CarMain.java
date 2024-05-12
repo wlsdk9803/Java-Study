ac62251424be019212a0a93ee3ff25bdf22aefce
kim nahyeon <stome6322@inha.edu>
2024-05-12 13:55:11 +0900

    [week4] : 상속 - 상속과 기능 추가

---
+++ java-basic-nahyeon/src/extends1/ex3/CarMain.java
@@ -0,0 +1,20 @@
+package extends1.ex3;
+
+public class CarMain {
+    public static void main(String[] args) {
+        ElectricCar electricCar = new ElectricCar();
+        electricCar.move();
+        electricCar.charge();
+        electricCar.openDoor();
+
+        GasCar gasCar = new GasCar();
+        gasCar.move();
+        gasCar.fillUp();
+        gasCar.openDoor();
+
+        HydrogenCar hydrogenCar = new HydrogenCar();
+        hydrogenCar.move();
+        hydrogenCar.openDoor(); // 부모 클래스의 기능 추가로 자식 클래스에서도 기능 확장
+        hydrogenCar.fillHydrogen();
+    }
+}
