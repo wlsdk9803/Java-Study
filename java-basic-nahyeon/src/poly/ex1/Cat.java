0e503dea2f996e5d5757bb40962506f247af2ae7
kim nahyeon <stome6322@inha.edu>
2024-05-20 01:25:47 +0900

    [week5] : 다형성2 - 다형성 활용

---
+++ java-basic-nahyeon/src/poly/ex1/Cat.java
@@ -0,0 +1,6 @@
+package poly.ex1;
+
+public class Cat {
+    public void sound() {
+        System.out.println("냐옹"); }
+}
---
+++ java-basic-nahyeon/src/poly/ex2/AnimalPolyMain2.java
@@ -0,0 +1,14 @@
+package poly.ex2;
+
+public class AnimalPolyMain2 {
+    public static void main(String[] args) {
+        Animal[] animalArr = {new Dog(), new Cat(), new Caw(), new Duck()};
+
+        for (Animal animal : animalArr) {
+            System.out.println("동물 소리 테스트 시작");
+            animal.sound();
+            System.out.println("동물 소리 테스트 종료");
+        }
+    }
+
+}
