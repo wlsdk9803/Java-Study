048513c4d55730eded492d89a422000d9558a264
colde99 <coldegg99@gmail.com>
2024-06-01 21:44:21 +0900

    local change first init

---
+++ java-basic-coldegg/week6/car0/K3Car.java
@@ -0,0 +1,16 @@
+package week6.car0;
+
+public class K3Car {
+
+    public void startEngine() {
+        System.out.printf("K3Car.startEngine");
+    }
+    public void offEngine() {
+        System.out.printf("K3Car.offEngine");
+    }
+    public void pressAccelerator() {
+        System.out.printf("K3Car.pressAccel");
+    }
+
+
+}
