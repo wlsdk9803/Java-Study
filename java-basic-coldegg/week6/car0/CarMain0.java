7582945947992ae8ce30d71142af55584ed82083
colde99 <coldegg99@gmail.com>
2024-06-02 14:28:53 +0900

    [update] c studyode 추가

--- java-basic-coldegg/week6/car0/CarMain0.java
+++ java-basic-coldegg/week6/car0/CarMain0.java
@@ -4,8 +4,13 @@ public class CarMain0 {
     public static void main(String[] args) {
         Driver coldegg = new Driver();
         K3Car k3_red = new K3Car();
-
-        coldegg.setK3(k3_red);
+//        coldegg.setK3(k3_red);
         coldegg.drive();
+
+        Model3Car model3_black = new Model3Car();
+//        coldegg.setK3(null);
+        coldegg.setmodel3(model3_black);
+        coldegg.drive();
+
     }
 }
