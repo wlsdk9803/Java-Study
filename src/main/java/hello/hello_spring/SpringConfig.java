06ebeb7d90f772e34163a2973cb1afd3e6d8d7dd
wlsdk9803 <wlsdk9803@naver.com>
2024-07-07 19:37:17 +0900

    섹션 4. 스프링 빈과 의존관계

---
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -0,0 +1,22 @@
+package hello.hello_spring;
+
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import hello.hello_spring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// 자바 코드로 직접 스프링 빈 등록 (이 방법 외에는 @Service와 같이 쓰는 컴포넌트 스캔이 있디.)
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+}
---
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -0,0 +1,44 @@
+package hello.hello_spring.controller;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+
+    // 생성자 주입 (의존성 주입에는 필드 주입, setter 주입, 생성자 주입이 있는데 보통 생성자 주입을 권장한다.)
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
