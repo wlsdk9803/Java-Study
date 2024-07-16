f446d9ee1fd4874aa09a66c7458756746b3194c8
wlsdk9803 <wlsdk9803@naver.com>
2024-07-16 18:32:47 +0900

    섹션 2. 스프링 핵심 원리 이해1 - 예제 만들기

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,18 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl(); // 구체화 된 MemberServiceImpl에 의존 => DIP 위반 (OCP도 위반하고 있음)
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find member = " + findMember.getName());
+    }
+}
